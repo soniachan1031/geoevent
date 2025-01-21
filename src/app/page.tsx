527442f144615a3ed049469da820457555e914a3
ridoy <ridoy51306@gmail.com>
2025-01-21 16:37:47 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800 text-white gap-5">
       <h1 className="text-3xl font-semibold">GeoEvent</h1>
-      <p className="text-lg">Coming soon!</p>
+      <p className="text-lg">Coming soon! some change</p>
     </div>
   );
 }
