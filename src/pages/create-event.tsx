60881fce12668773e9ba244fd843f447be2ff4bc
ridoy <ridoy51306@gmail.com>
2025-03-17 19:50:15 -0400

    fix: remove success toast notification from event creation process

--- src/pages/create-event.tsx
+++ src/pages/create-event.tsx
@@ -3,13 +3,11 @@ import serverSidePropsHandler from "@/lib/server/serverSidePropsHandler";
 import { EApiRequestMethod } from "@/types/api.types";
 import { EAuthStatus } from "@/types/user.types";
 import { useRouter } from "next/navigation";
-import toast from "react-hot-toast";
 
 export default function CreateEvent() {
   const router = useRouter();
 
   const onSuccess = () => {
-    toast.success("Event created successfully");
     router.push("/my-hosted-events");
   };
 
