39e33aa32cac64ef0cc606056e5feb8a20c8c14f
ridoy <ridoy51306@gmail.com>
2025-03-17 20:38:44 -0400

    fix: streamline JSX formatting in MyRegisteredEvents component

--- src/pages/my-saved-events/index.tsx
+++ src/pages/my-saved-events/index.tsx
@@ -17,16 +17,11 @@ export default function MyRegisteredEvents({
       />
       <h1 className="text-3xl">My Saved Events</h1>
       {events.length === 0 ? (
-        <p className="text-gray-500">
-          You haven&apos;t saved any events yet.
-        </p>
+        <p className="text-gray-500">You haven&apos;t saved any events yet.</p>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full max-w-7xl">
           {events.map((event) => (
-            <EventCard
-              key={event._id}
-              event={event}
-            />
+            <EventCard key={event._id} event={event} />
           ))}
         </div>
       )}
